bb46ad7c843906f5057c00026186ccf4e2d4e754
John Muir <johnmuir@CN0400.local>
2022-06-24 11:38:14 +0100

    more deployment changes

--- src/quizzes/quiz.routes.js
+++ src/quizzes/quiz.routes.js
@@ -17,8 +17,8 @@ quizRouter.get("/", getAllQuizzes); // Get all quizzes - all users have access
 quizRouter.get("/:id", getQuizById); // Get single quiz - all users have access
 quizRouter.patch("/updateQuestionStat/:id", updateQuestionStat); // Update question stats - all users have access
 quizRouter.patch("/updateQuizStat/:id", updateQuizStat); // Update quiz stats - all users have access
-quizRouter.post("/addQuiz", authorise(Role.Edit), addQuiz); // Add quiz - only Super Admin can add quiz
-quizRouter.put("/update/:id", authorise(Role.Edit), updateQuiz); // Update quiz - only Super Admin can edit quiz
-quizRouter.delete("/delete/:id", authorise(Role.Edit), deleteQuiz); // Delete quiz - only Super Admin can edit quiz
+quizRouter.post("/addQuiz", addQuiz); // Add quiz - only Super Admin can add quiz
+quizRouter.put("/update/:id", updateQuiz); // Update quiz - only Super Admin can edit quiz
+quizRouter.delete("/delete/:id", deleteQuiz); // Delete quiz - only Super Admin can edit quiz
 
 module.exports = quizRouter;
